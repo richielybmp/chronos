9c50511c6f19c233923e5dd6cfcfb3f557d3d6a3
Richiely Paiva <richielybmp@hotmail.com>
2019-10-01 20:22:33 -0300

    Home banner

--- web/src/app/shared/components/homeContainer/HomeSlider.tsx
+++ web/src/app/shared/components/homeContainer/HomeSlider.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Slider from "react-slick";
 import { Image, Header } from 'semantic-ui-react';
-import cronogramas from '../../../../assets/images/cronogramas.png'
+import cronogramas from '../../../../assets/images/banner_cronogramas.png'
 import cronograma from '../../../../assets/images/cronograma.png'
 import artefato from '../../../../assets/images/artefato.png'
+
+import "./sliderhome.css"
 
 export default function HomeSlider() {
     return (
@@ -12,26 +14,11 @@ export default function HomeSlider() {
                 Crie! Organize! Acompanhe! Se torne melhor!
             </Header>
             <Slider {...SliderSettings()} className="my-slider">
-                <div className="center-content border-home-image">
-                    <Image
-                        as='div'
-                        size='massive'
-                        src={cronogramas}
-                    />
+                <div className="banner b1">
                 </div>
-                <div className="center-content border-home-image">
-                    <Image
-                        as='div'
-                        size='massive'
-                        src={cronograma}
-                    />
+                <div className="banner b2">
                 </div>
-                <div className="center-content border-home-image">
-                    <Image
-                        as='div'
-                        size='massive'
-                        src={artefato}
-                    />
+                <div className="banner b3">
                 </div>
             </Slider>
         </>
@@ -44,7 +31,7 @@ function SliderSettings() {
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 3000 // 5 segundos
     };
 
